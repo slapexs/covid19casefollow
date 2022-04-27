ee8fe824c6bbd558a4b205893733869c43b8e382
onze <slapexs@gmail.com>
2022-04-27 22:42:29 +0700

    Add main navbar

---
+++ components/navbar.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="./">หน้าแรก</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainnavbar" aria-controls="mainnavbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="mainnavbar">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Link 1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link 2</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                        username
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#"><i class="ri-user-line"></i> แก้ไขโปรไฟล์</a>
+                        <a class="dropdown-item" href="#"><i class="ri-lock-password-line"></i> แก้ไขรหัสผ่าน</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#"><i class="ri-logout-box-line"></i> ออกจากระบบ</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -10,14 +11,21 @@
     <link rel="stylesheet" href="./assets/css/style.css">
     <!-- Favicon -->
     <link rel="shortcut icon" href="./assets/favicon/virus.png" type="image/x-icon">
+    <!-- Remixicon -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <title>ระบบบันทึกและติดตามเคสผู้ป่วยโควิด-19 รพ.สต.ร้องกวาง</title>
-  </head>
-  <body>
+</head>
+
+<body>
+    <?php
+    include_once('./components/navbar.php');
+    ?>
 
 
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 
-  </body>
+</body>
+
 </html>
