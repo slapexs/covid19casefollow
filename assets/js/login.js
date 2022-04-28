f586feb06210d452326b78f663263c0b98fee564
onze <slapexs@gmail.com>
2022-04-28 22:32:52 +0700

    Add alert if password login invalid

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,16 +1,18 @@
-$("#formlogin").submit((e) => {
+$('#formlogin').submit((e) => {
   e.preventDefault();
-  const logindata = [$("#m_username").val(), $("#m_password").val()];
+  const logindata = [$('#m_username').val(), $('#m_password').val()];
   $.ajax({
-    url: "./backend/function.php",
-    type: "POST",
+    url: './backend/function.php',
+    type: 'POST',
     data: {
       checklogin: logindata,
     },
-    dataType: "JSON",
+    dataType: 'JSON',
     success: (res) => {
-      if (res.msg == "success") {
-        window.location.href = "./";
+      if (res.msg == 'success') {
+        window.location.href = './';
+      } else {
+        alert('รหัสผ่านไม่ถูกต้อง');
       }
     },
     error: (err) => console.log(err),
