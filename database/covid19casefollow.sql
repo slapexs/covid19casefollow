f130e084bb91b41ac394964c7db5d11b9c76bc44
onze <slapexs@gmail.com>
2022-05-02 08:59:02 +0700

    Update file database

--- database/covid19casefollow.sql
+++ database/covid19casefollow.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 01, 2022 at 09:20 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Host: localhost:8889
+-- Generation Time: May 02, 2022 at 01:58 AM
+-- Server version: 5.7.34
+-- PHP Version: 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,17 +37,20 @@ CREATE TABLE `cases` (
   `c_address` text NOT NULL,
   `c_phone` varchar(10) NOT NULL,
   `c_detail` text NOT NULL,
+  `c_note` text NOT NULL,
   `c_start_quarantine` date NOT NULL,
   `c_end_quarantine` date NOT NULL,
-  `c_timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `c_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `c_status` int(1) NOT NULL DEFAULT '0' COMMENT '0=in progress, 1=closed'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `cases`
 --
 
-INSERT INTO `cases` (`c_id`, `c_ref_docid`, `c_village_num`, `c_fname`, `c_lname`, `c_cardid`, `c_address`, `c_phone`, `c_detail`, `c_start_quarantine`, `c_end_quarantine`, `c_timestamp`) VALUES
-('010522-31FJP', 2, 1, 'fname', 'lname', '1541234567891', '203 ต.ร้องกวาง อ.ร้องกวาง จ.แพร่ 54140', '0641234567', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2022-05-01', '2022-05-10', '2022-05-01 04:20:31');
+INSERT INTO `cases` (`c_id`, `c_ref_docid`, `c_village_num`, `c_fname`, `c_lname`, `c_cardid`, `c_address`, `c_phone`, `c_detail`, `c_note`, `c_start_quarantine`, `c_end_quarantine`, `c_timestamp`, `c_status`) VALUES
+('010522-31FJP', 2, 1, 'fname', 'lname', '1541234567891', '203 ต.ร้องกวาง อ.ร้องกวาง จ.แพร่ 54140', '0641234567', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '', '2022-05-01', '2022-05-10', '2022-05-01 04:20:31', 0),
+('010522-5YVSQ', 2, 5, 'fname', 'lname', '1231231231231', '123 ม.5 ต.ร้องกวาง อ.ร้องกวาง จ.แพร่ 54140', '0981231233', 'แฟลชเนอะ เหี่ยวย่น ไมค์ ไกด์แอดมิสชัน ออดิชั่นกิฟท์คาราโอเกะอัลบัมตัวตน อุด้งดิสเครดิตทับซ้อนเดอะบัตเตอร์ เช็งเม้ง เวิร์กแชเชือนซัพพลายโอเพ่น ตังค์ลิสต์คาร์โก้ไพลินกรอบรูป แมชชีนละติน คอมเพล็กซ์อิกัวนานู้ดเอาต์แป๋ว แกรนด์ เก๋ากี้อุเทนฮอตดอกว้าว เปราะบางดาวน์ตังค์ยิวบู๊ พุทโธแคทวอล์คแครอทซัมเมอร์ดีพาร์ตเมนต์ เอฟเฟ็กต์มหภาคตุ๊ดซิลเวอร์', 'เฟอร์นิเจอร์ป๊อกเทียมทานเอ๋ ก๋ากั่นมัฟฟินไชน่าเซ่นไหว้ คอลัมน์วอร์รูมแตงกวามลภาวะ บุญคุณฮาร์ดหลินจือเปโซ เปเปอร์อวอร์ดฟีเวอร์โพลารอยด์ แพนดาไชน่า ชิฟฟอนรากหญ้าแต๋วซูโม่ แหววอัลมอนด์มายองเนสดีพาร์ตเมนต์กุมภาพันธ์', '2022-05-01', '2022-05-11', '2022-05-01 11:38:48', 1);
 
 -- --------------------------------------------------------
 
@@ -61,7 +64,7 @@ CREATE TABLE `members` (
   `m_password` text NOT NULL,
   `m_fname` varchar(255) NOT NULL,
   `m_lname` varchar(255) NOT NULL,
-  `m_role` int(1) NOT NULL DEFAULT 0 COMMENT '0=อสม, 1=แพทย์, 2=แอดมิน'
+  `m_role` int(1) NOT NULL DEFAULT '0' COMMENT '0=อสม, 1=แพทย์, 2=แอดมิน'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
