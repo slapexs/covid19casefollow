b8e12f432e0d15a331bed408a215554bba7dee7f
onze <slapexs@gmail.com>
2022-05-10 19:39:14 +0700

    Update file backup database

--- database/covid19casefollow.sql
+++ database/covid19casefollow.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: May 02, 2022 at 01:58 AM
--- Server version: 5.7.34
--- PHP Version: 7.4.21
+-- Host: 127.0.0.1
+-- Generation Time: May 10, 2022 at 02:38 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,8 +40,8 @@ CREATE TABLE `cases` (
   `c_note` text NOT NULL,
   `c_start_quarantine` date NOT NULL,
   `c_end_quarantine` date NOT NULL,
-  `c_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `c_status` int(1) NOT NULL DEFAULT '0' COMMENT '0=in progress, 1=closed'
+  `c_timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `c_status` int(1) NOT NULL DEFAULT 0 COMMENT '0=in progress, 1=closed,\r\n2 = death'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -49,8 +49,12 @@ CREATE TABLE `cases` (
 --
 
 INSERT INTO `cases` (`c_id`, `c_ref_docid`, `c_village_num`, `c_fname`, `c_lname`, `c_cardid`, `c_address`, `c_phone`, `c_detail`, `c_note`, `c_start_quarantine`, `c_end_quarantine`, `c_timestamp`, `c_status`) VALUES
-('010522-31FJP', 2, 1, 'fname', 'lname', '1541234567891', '203 ต.ร้องกวาง อ.ร้องกวาง จ.แพร่ 54140', '0641234567', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '', '2022-05-01', '2022-05-10', '2022-05-01 04:20:31', 0),
-('010522-5YVSQ', 2, 5, 'fname', 'lname', '1231231231231', '123 ม.5 ต.ร้องกวาง อ.ร้องกวาง จ.แพร่ 54140', '0981231233', 'แฟลชเนอะ เหี่ยวย่น ไมค์ ไกด์แอดมิสชัน ออดิชั่นกิฟท์คาราโอเกะอัลบัมตัวตน อุด้งดิสเครดิตทับซ้อนเดอะบัตเตอร์ เช็งเม้ง เวิร์กแชเชือนซัพพลายโอเพ่น ตังค์ลิสต์คาร์โก้ไพลินกรอบรูป แมชชีนละติน คอมเพล็กซ์อิกัวนานู้ดเอาต์แป๋ว แกรนด์ เก๋ากี้อุเทนฮอตดอกว้าว เปราะบางดาวน์ตังค์ยิวบู๊ พุทโธแคทวอล์คแครอทซัมเมอร์ดีพาร์ตเมนต์ เอฟเฟ็กต์มหภาคตุ๊ดซิลเวอร์', 'เฟอร์นิเจอร์ป๊อกเทียมทานเอ๋ ก๋ากั่นมัฟฟินไชน่าเซ่นไหว้ คอลัมน์วอร์รูมแตงกวามลภาวะ บุญคุณฮาร์ดหลินจือเปโซ เปเปอร์อวอร์ดฟีเวอร์โพลารอยด์ แพนดาไชน่า ชิฟฟอนรากหญ้าแต๋วซูโม่ แหววอัลมอนด์มายองเนสดีพาร์ตเมนต์กุมภาพันธ์', '2022-05-01', '2022-05-11', '2022-05-01 11:38:48', 1);
+('010522-31FJP', 2, 1, 'fname', 'lname', '1541234567891', '203 ต.ร้องกวาง อ.ร้องกวาง จ.แพร่ 54140', '0641234567', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '', '2022-05-01', '2022-05-10', '2022-05-09 03:01:35', 1),
+('010522-5YVSQ', 2, 5, 'fname', 'lname', '1231231231231', '123 ม.5 ต.ร้องกวาง อ.ร้องกวาง จ.แพร่ 54140', '0981231233', 'แฟลชเนอะ เหี่ยวย่น ไมค์ ไกด์แอดมิสชัน ออดิชั่นกิฟท์คาราโอเกะอัลบัมตัวตน อุด้งดิสเครดิตทับซ้อนเดอะบัตเตอร์ เช็งเม้ง เวิร์กแชเชือนซัพพลายโอเพ่น ตังค์ลิสต์คาร์โก้ไพลินกรอบรูป แมชชีนละติน คอมเพล็กซ์อิกัวนานู้ดเอาต์แป๋ว แกรนด์ เก๋ากี้อุเทนฮอตดอกว้าว เปราะบางดาวน์ตังค์ยิวบู๊ พุทโธแคทวอล์คแครอทซัมเมอร์ดีพาร์ตเมนต์ เอฟเฟ็กต์มหภาคตุ๊ดซิลเวอร์', 'เฟอร์นิเจอร์ป๊อกเทียมทานเอ๋ ก๋ากั่นมัฟฟินไชน่าเซ่นไหว้ คอลัมน์วอร์รูมแตงกวามลภาวะ บุญคุณฮาร์ดหลินจือเปโซ เปเปอร์อวอร์ดฟีเวอร์โพลารอยด์ แพนดาไชน่า ชิฟฟอนรากหญ้าแต๋วซูโม่ แหววอัลมอนด์มายองเนสดีพาร์ตเมนต์กุมภาพันธ์', '2022-05-01', '2022-05-11', '2022-05-08 15:19:16', 2),
+('080522-5ONNL', 5, 13, 'Dana', 'Perry', '9302793027123', '123 ม.13 ต.ร้องกวาง อ.ร้องกวาง จ.แพร่ 54140', '0119627516', 'dc523cb313b63dfe5be2140b0c05b3bc', '7a4aa07d1bedcc6bcf4b7f8856643492c191540d', '2022-05-08', '2022-05-15', '2022-05-09 01:35:45', 0),
+('090522-7HGWB', 2, 11, 'Norax', 'Davis', '2002200220021', '962/1 ม.11 ต.ร้องกวาง อ.ร้องกวาง จ.แพร่ 54140', '0814540666', 'dc523cb313b63dfe5be2140b0c05b3bc', '7a4aa07d1bedcc6bcf4b7f8856643492c191540d', '2022-05-09', '2022-05-16', '2022-05-09 04:42:51', 0),
+('090522-7PJ0J', 2, 12, 'Tony', 'Bates', '9267926792671', '321/12 ม.12 ต.ร้องกวาง อ.ร้องกวาง จ.แพร่ 54140', '0123456789', 'dc523cb313b63dfe5be2140b0c05b3bc', '7a4aa07d1bedcc6bcf4b7f8856643492c191540d', '2022-05-09', '2022-05-16', '2022-05-09 03:05:25', 0),
+('090522-Z1WUE', 5, 12, 'Bobbie', 'Carpenter', '7240724072401', '456 ม.12 ต.ร้องกวาง อ.ร้องกวาง จ.แพร่ 54140', '0814540666', 'dc523cb313b63dfe5be2140b0c05b3bc', '7a4aa07d1bedcc6bcf4b7f8856643492c191540d', '2022-05-09', '2022-05-16', '2022-05-09 02:02:36', 0);
 
 -- --------------------------------------------------------
 
@@ -64,7 +68,7 @@ CREATE TABLE `members` (
   `m_password` text NOT NULL,
   `m_fname` varchar(255) NOT NULL,
   `m_lname` varchar(255) NOT NULL,
-  `m_role` int(1) NOT NULL DEFAULT '0' COMMENT '0=อสม, 1=แพทย์, 2=แอดมิน'
+  `m_role` int(1) NOT NULL DEFAULT 0 COMMENT '0=อสม, 1=แพทย์, 2=แอดมิน'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -74,8 +78,8 @@ CREATE TABLE `members` (
 INSERT INTO `members` (`m_id`, `m_username`, `m_password`, `m_fname`, `m_lname`, `m_role`) VALUES
 (1, 'admin', '$2y$10$oPZeBYpDDWbHO4RD/2oun.zydgogwpK8bWiWevGocvAALDmzwA1M.', 'Admins', 'admin', 2),
 (2, 'doctor', '$2y$10$46fx1pUBDMQaG2mWS.xLyOxMdNWiNVGp6hLtZQBn9yRlcNH5HB8tG', 'Doctor', 'lastname', 1),
-(3, 'staff', '', 'Staff', 'lastname', 0),
-(5, 'tester', '$2y$10$bqdAAusBjycDND8fTawUdOmJRnhpbRp3bAohnOD4iYoMj1XyyRZ4m', 'Tinngrit', 'Singkaew', 2);
+(3, 'staff', '$2y$10$9LviJL9.58Hdp66LNQiSoeWirehWlzHjE4H.Fn8z14pUN.DvOZj0C', 'Staff', 'lastname', 0),
+(5, 'tester', '$2y$10$bqdAAusBjycDND8fTawUdOmJRnhpbRp3bAohnOD4iYoMj1XyyRZ4m', 'Tinngrit', 'Singkaew', 1);
 
 --
 -- Indexes for dumped tables
